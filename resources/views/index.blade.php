675401b865a83cb542864b6a88cf5486f274126c
abbas0506 <abbas.sscs@gmail.com>
2022-12-25 15:11:33 +0500

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title</title>
+    @vite('resources/css/app.css')
+    <!-- <link rel="stylesheet" href="{{asset('/build/assets/app.css')}}">
+    <script src="{{asset('/build/assets/app.js')}}"></script> -->
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline text-pink-400">
+        Hello world!
+    </h1>
+</body>
+
+</html>
