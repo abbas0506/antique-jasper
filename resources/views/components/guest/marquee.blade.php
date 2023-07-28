bdcfdfef2a256e270a807a152bdf809086c75390
abbas0506 <abbas.sscs@gmail.com>
2023-07-28 18:53:33 +0500

    28072306

--- resources/views/components/guest/marquee.blade.php
+++ resources/views/components/guest/marquee.blade.php
@@ -9,10 +9,13 @@
                 <i class="bi bi-whatsapp"></i>
                 +92 304 4933477
             </a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-twitter"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-
+            <a href="https://web.facebook.com/AntiqueJasper?_rdc=1&_rdr" target="_blank"><i class="bi bi-facebook"></i></a>
+            <a href="https://www.instagram.com/antique_jasper/" target="_blank"><i class="bi bi-instagram text-slate-300"></i></a>
+            <a href="https://www.tiktok.com/@antiquejasper2" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16">
+                    <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z" />
+                </svg>
+            </a>
         </div>
     </div>
 </div>
