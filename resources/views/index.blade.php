2058a5018bae32f5ab0aaa9befa6124db1392d7d
abbas0506 <abbas.sscs@gmail.com>
2023-08-04 14:41:32 +0500

    04082304

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,20 @@
 
 @section('body')
 <!-- hero section -->
-<section class="h-screen bg-orange-50 bg-cover bg-no-repeat">
+@php
+$url=asset('images/ring.png');
+@endphp
+<section class="h-screen bg-orange-50 bg-right-bottom bg-no-repeat" style="background-image: url('{{$url}}');">
     <x-guest.marquee></x-guest.marquee>
     <div class="container h-full">
-        <div class="grid grid-cols-1 md:grid-cols-2 h-full items-center">
+        <div class="flex w-full md:w-1/2 h-full items-center">
             <div>
-                <h1>ANTIQUE JASPER</h1>
+                <h1 class="text-center md:text-left">ANTIQUE JASPER</h1>
                 <p class="text-xl mt-6">We have a unique collection of 1000+ items for you with reasonable price. We believe in our prolonged customer relations. Feel free to place your order; shipment is free all over Pakistan.</p>
             </div>
-            <div class="text-center">
-                image
-            </div>
+            <!-- <div class="text-center">
+                <img src="{{asset('images/ring.png')}}" alt="ring">
+            </div> -->
         </div>
         <!-- <div class="flex flex-wrap-reverse items-center justify-between h-full">
 
@@ -105,11 +108,17 @@
                 $url=asset('images/products')."/".$product->image;
                 @endphp
                 <div class="product-card">
-                    <a href="{{route('articles.show',$product)}}">
+                    <a href="{{route('articles.show',$product)}}" class="hover:cursor-pointer">
                         <div class="img-container">
-                            <img src="{{$url}}" alt="" class="bg-cover">
+                            <div class="bg-img-hover-scale" style="background-image: url('{{asset($url)}}');"></div>
+                            <div class="cart-icon">
+                                <a href="{{route('cart.add', $product->id)}}">
+                                    <i class="bi-cart2 hover:text-blue-600"></i>
+                                </a>
+                                <!-- <i class="bi-heart hover:text-red-500 text-[14px]"></i> -->
+                            </div>
                         </div>
-                        <div class="product-desc mt-2">
+                        <div class="product-desc p-2">
 
                             @php
                             if(strlen($product->name)>30)
@@ -118,29 +127,9 @@
                             $reducedName=$product->name;
                             @endphp
 
-
                             <p class="text-xs md:text-sm text-slate-900">{{$reducedName}} </p>
-                            <p class="mt-1 text-xs md:text-sm text-slate-600">AJ#{{$product->code}} &nbsp PKR {{$product->price}}</p>
-
-                            <!-- <div class="flex items-center">
-                                <a href="{{route('cart.add', $product->id)}}" class="add2cart">
-                                    <i class="bi bi-cart2"></i>
-                                </a>
-                            </div> -->
+                            <p class="mt-1 text-xs  text-slate-600">AJ#{{$product->code}} &nbsp PKR {{$product->price}}</p>
                         </div>
-
-
-                        <!-- <div class="add-to-cart">
-                            <div>
-                                <a href="{{route('articles.show',$product)}}">{{$product->name}} </a>
-                                <p>AJ#{{$product->code}}, &nbsp Rs. {{$product->price}}</p>
-                            </div>
-                            <div class="flex items-center">
-                                <a href="{{route('cart.add', $product->id)}}" class="add2cart">
-                                    <i class="bi bi-cart2"></i>
-                                </a>
-                            </div>
-                        </div> -->
                     </a>
                 </div>
 
